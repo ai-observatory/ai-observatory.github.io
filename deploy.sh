428ce661a8d718c06f1723193e704aba256c7786
prtysh <prtyshrmn@gmail.com>
2020-12-18 04:03:57 +0530

    before sleep

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:prtysh/algosite2.git master:gh-pages
+git push -f git@github.com:ai-observatory/ai-observatory.github.io.git master:gh-pages
 cd -
